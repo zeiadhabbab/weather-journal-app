92678a9b3b5d1c64257b5f590e6e2d008f13a0d4
Zeyad Habbab <zeiad.habbab@live.com>
2024-07-14 13:32:28 +0300

    Add All new code

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+//Importing all require modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
 // Require Express to run server and routes
+const app = express();
+
 
 // Start up an instance of app
 
@@ -11,9 +19,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log(`server running on localhost: ${port}`);
+}
+
+// Callback function to complete GET '/get-weather-data'
+function getWeatherData(req, res) {
+    res.send(projectData);
+}
+
+// Callback function to complete POST '/post-weather-data'
+function postWeatherData(req, res) {
+    const { zipCode, feelingsContent, temp, date, city, country, icon, weatherDetails } = req.body;
+    projectData = { zipCode, feelingsContent, temp, date, city, country, icon, weatherDetails };
+    console.log("Weather data has been saved.");
+    console.log(projectData);
+    res.send({ message: "Entry has been saved." });
+}
+
+// GET route
+app.get('/get-weather-data', getWeatherData);
+
+// POST route
+app.post('/post-weather-data', postWeatherData);
--- website/app.js
+++ website/app.js
@@ -1,5 +1,117 @@
 /* Global Variables */
+const WEATHER_API_KEY = 'dc17e11e845722c9c950a50c6d5c3b79';
+const WEATHER_API_UNITS = 'metric';
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// DOM elements
+const zipInput = document.getElementById("zip");
+const feelingsInput = document.getElementById("feelings");
+const entryHolder = document.getElementById("entryHolder");
+const dateDisplayer = document.getElementById("date");
+const tempDisplayer = document.getElementById("temp");
+const contentDisplayer = document.getElementById("content");
+const getDataButton = document.getElementById("generate");
+const errorMsg = document.getElementById("error");
+const citylocation = document.getElementById("location");
+const desc = document.getElementById("desc");
+const loader = document.getElementById("loader");
+
+// Function to update web page with recent entry / cashed entry
+const changeWebPage = async () => {
+    // Clearing the error message
+    errorMsg.textContent = "";
+    loader.classList.remove("hidden");
+
+
+    try {
+        const response = await fetch('/get-weather-data');
+        if (!response.ok) {
+            throw new Error(`Failed to fetch recent entry: ${response.status}`);
+        }
+
+        const { date, temp, feelingsContent, city, country, weatherDetails, icon } = await response.json();
+        dateDisplayer.textContent = `Date: ${date}`;
+        tempDisplayer.textContent = `Temp: ${Math.round(temp)} degrees`;
+        desc.textContent = `Weather: ${weatherDetails}`;
+        contentDisplayer.textContent = `Feeling today: ${feelingsContent}`;
+        citylocation.textContent = `Location: ${city}, ${country}`;
+
+
+        //Displaying the wether icon
+        entryHolder.style.backgroundImage = `url(${icon})`;
+        loader.classList.add("hidden");
+
+        
+    } catch (error) {
+        console.error("Error updating UI:", error);
+        loader.classList.add("hidden");
+    }
+};
+
+// Function to handle generate button click event
+const getDataButtonListener = async (event) => {
+    event.preventDefault();
+    loader.classList.remove("hidden");
+
+    const zip = zipInput.value.trim();
+    const feelings = feelingsInput.value.trim();
+    const date = new Date();
+
+
+    if (!zip || !feelings) {
+        errorMsg.textContent = "Please fill the zip code and feelings input.";
+        return;
+    }
+
+    try {
+        const weatherDetails = await getWeatherData(zip);
+        const postData = {
+            zipCode: zip,
+            feelingsContent: feelings,
+            temp: weatherDetails.main.temp,
+            city: weatherDetails.name,
+            country: weatherDetails.sys.country,
+            weatherDetails: weatherDetails.weather[0].description,
+            icon: 'https://openweathermap.org/img/wn/'+  weatherDetails.weather[0].icon +'@2x.png',
+            date: `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+        };
+
+        await fetch("/post-weather-data", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(postData)
+        });
+
+        await changeWebPage();
+    } catch (error) {
+        console.error("Error generating entry:", error);
+        if (error.message === "city not found") {
+            errorMsg.textContent = "No city found with the given zip code. Please enter a valid zip code with Contry code.";
+        }
+    }
+};
+
+// Function to fetch weather data from API OpenWeatherMap API
+const getWeatherData = async (zipCode) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${WEATHER_API_KEY}&units=${WEATHER_API_UNITS}`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            errorMsg.textContent = "Failed to fetch data. Please try again.";
+            throw new Error(`Failed to fetch data: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        throw error;
+        errorMsg.textContent = "Failed to fetch data. Please try again.";
+    }
+};
+
+
+// Initialize UI when DOM content is loaded
+window.addEventListener("DOMContentLoaded", changeWebPage);
+
+// Add event listener to get data button
+getDataButton.addEventListener('click', getDataButtonListener);
